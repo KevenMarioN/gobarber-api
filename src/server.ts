04db70327eb8956ea07e8da50cdc4136b8ef31b1
KevenMarioN <kevenmario.n.r@gmail.com>
2022-02-15 07:08:53 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import appointments from './routes/appointments.routes';
+
+const app = express();
+app.use(express.json());
+app.use('/appointments', appointments);
+
+app.listen(3333, () => {
+  console.log('🚀 Server running in port 3333!');
+});
