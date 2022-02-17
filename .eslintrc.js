22c49140db2467fef85d2df544470e14a4595184
KevenMarioN <kevenmario.n.r@gmail.com>
2022-02-17 12:44:39 -0300

    feat: Appointment class

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     "@typescript-eslint"
   ],
   "rules": {
+    "max-len" : "",
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": [
       "error"
