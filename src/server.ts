dc3cce19c3332c084d98ea16cbdc3108a03a4621
KevenMarioN <kevenmario.n.r@gmail.com>
2022-05-16 10:55:26 -0300

    feat: :sparkles: cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import './database';
 
@@ -8,6 +9,8 @@ import routes from './routes';
 import AppError from './errors/AppError';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
